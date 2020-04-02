faaa00a54406add65ac4b6213e7c06fb101ae1a7
sbenita <shlomi.benita@cyberark.com>
2020-04-02 09:45:48 +0300

    Bump version + minor bug fixes

--- saml2art/exporter/netrc.py
+++ saml2art/exporter/netrc.py
@@ -5,8 +5,12 @@ import os
 class NetrcExporter:
     def export(self, path, host, username, password):
         print("Updating [%s]..." % path, end="")
-        netrc_file = netrc.netrc(os.path.expanduser(path))
+        netrc_file_path = os.path.expanduser(path)
+        if os.path.exists(netrc_file_path):
+            netrc_file = netrc.netrc(netrc_file_path)
+        else:
+            netrc_file = netrc.netrc()
         netrc_file.hosts[host] = (username, None, password)
-        with open(os.path.expanduser(path), "w") as file:
+        with open(os.path.expanduser(path), "w+") as file:
             file.write(netrc_file.__repr__())
         print("Done!")
